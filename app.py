3ac85c7f938e158e1c796e8d2bf050b697524a77
Darryl Yan <darryl.yan.dy@gmail.com>
2017-03-26 18:33:28 -0400

    Init commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import models
+import forms
+
+app = Flask(__name__)
+app.secret_key = 's3cr3t'
+app.config.from_object('config')
+db = SQLAlchemy(app, session_options={'autocommit': False})
+
+@app.route('/')
+def all_drinkers():
+    drinkers = db.session.query(models.Drinker).all()
+    return render_template('all-drinkers.html', drinkers=drinkers)
+
+@app.route('/drinker/<name>')
+def drinker(name):
+    drinker = db.session.query(models.Drinker)\
+        .filter(models.Drinker.name == name).one()
+    return render_template('drinker.html', drinker=drinker)
+
+@app.route('/edit-drinker/<name>', methods=['GET', 'POST'])
+def edit_drinker(name):
+    drinker = db.session.query(models.Drinker)\
+        .filter(models.Drinker.name == name).one()
+    beers = db.session.query(models.Beer).all()
+    bars = db.session.query(models.Bar).all()
+    form = forms.DrinkerEditFormFactory.form(drinker, beers, bars)
+    if form.validate_on_submit():
+        try:
+            form.errors.pop('database', None)
+            models.Drinker.edit(name, form.name.data, form.address.data,
+                                form.get_beers_liked(), form.get_bars_frequented())
+            return redirect(url_for('drinker', name=form.name.data))
+        except BaseException as e:
+            form.errors['database'] = str(e)
+            return render_template('edit-drinker.html', drinker=drinker, form=form)
+    else:
+        return render_template('edit-drinker.html', drinker=drinker, form=form)
+
+@app.template_filter('pluralize')
+def pluralize(number, singular='', plural='s'):
+    return singular if number in (0, 1) else plural
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ config.py
@@ -0,0 +1,3 @@
+SQLALCHEMY_DATABASE_URI = 'postgresql://vagrant:dbpasswd@localhost/beers'
+SQLALCHEMY_ECHO = True
+DEBUG = True
